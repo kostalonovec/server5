3bc8f3e566a17ca4b5f68acd5f44300f4ce10ece
Jan Sebastián Kostlán <78684413+kostalonovec@users.noreply.github.com>
2022-05-08 17:18:29 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,52 @@
+radio.set_group(15)
+odpovedi = [{"serial" : 156840, "vote": 0}]
+odpovedi.pop()
+muzehlasovat = True
+
+radio.on_received_value(on_received_value)
+
+def on_received_value(name, value):
+    global odpovedi, muzehlasovat
+    if name == "answer" and muzehlasovat:
+        zmeneno = False
+        serial_num = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+        counter = -1
+        
+        for o in odpovedi:
+            counter += 1
+            if o["serial"] == serial_num:
+                odpovedi[counter]["vote"] = value
+                zmeneno = True
+        if not zmeneno:
+            odpovedi.push({"serial" : serial_num, "vote": value})
+
+        radio.send_value("prijat", serial_num)
+        #klient může odpovídat
+
+def on_button_pressed_a():
+    global muzehlasovat
+
+    if muzehlasovat:
+        muzehlasovat = False
+    else:
+        muzehlasovat = True
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+
+    for i in range (0,5):
+        pocet = 0
+        for p in odpovedi:
+            if p["vote"] == i:
+                pocet += 1
+        print(String.from_char_code(i+65) + ": " + pocet)
+        basic.show_string(String.from_char_code(i+65) + ":" + pocet, 50)
+
+        #zobrazování odpovědí
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_logo_event_pressed():
+    for i in range(odpovedi.length):
+        odpovedi.remove_at(i)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
---
+++ main.ts
@@ -0,0 +1,67 @@
+radio.setGroup(15)
+let odpovedi = [ {
+    "serial" : 156840,
+    "vote" : 0,
+}
+]
+_py.py_array_pop(odpovedi)
+let muzehlasovat = true
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    let zmeneno: boolean;
+    let serial_num: number;
+    let counter: number;
+    
+    if (name == "answer" && muzehlasovat) {
+        zmeneno = false
+        serial_num = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+        counter = -1
+        for (let o of odpovedi) {
+            counter += 1
+            if (o["serial"] == serial_num) {
+                odpovedi[counter]["vote"] = value
+                zmeneno = true
+            }
+            
+        }
+        if (!zmeneno) {
+            odpovedi.push( {
+                "serial" : serial_num,
+                "vote" : value,
+            }
+            )
+        }
+        
+        radio.sendValue("prijat", serial_num)
+    }
+    
+})
+// klient může odpovídat
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (muzehlasovat) {
+        muzehlasovat = false
+    } else {
+        muzehlasovat = true
+    }
+    
+})
+// zobrazování odpovědí
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    let pocet: number;
+    for (let i = 0; i < 5; i++) {
+        pocet = 0
+        for (let p of odpovedi) {
+            if (p["vote"] == i) {
+                pocet += 1
+            }
+            
+        }
+        console.log(String.fromCharCode(i + 65) + ": " + pocet)
+        basic.showString(String.fromCharCode(i + 65) + ":" + pocet, 50)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    for (let i = 0; i < odpovedi.length; i++) {
+        odpovedi.removeAt(i)
+    }
+})
